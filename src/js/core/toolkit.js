d9a08b48e9707ad3c9c31ea46860b0f3bd56dffb
patient <1923407743@qq.com>
2018-10-03 00:13:49 +0800

    generator

--- src/js/core/toolkit.js
+++ src/js/core/toolkit.js
@@ -23,14 +23,53 @@ const matrixToolkit = {
     }
     return array;
   },
+  /**
+   * todo 检查指定位置是否可填写
+   */
+  checkFillable(matrix, n, rowIndex, colIndex) {
+    const row = matrix[rowIndex];
+    const column = this.makeRow().map((v, i) => matrix[i][colIndex]);
+    const { boxIndex } = boxToolkit.covertToBoxIndex(rowIndex, colIndex);
+    const box = boxToolkit.getBoxCells(matrix, boxIndex);
+    for (let i = 0; i < 9; i++) {
+      if (row[i] === n || column[i] === n || box[i] === n) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 /**
  * 宫坐标系工具
  */
 
-const boxToolit = {
-  // Todo
+const boxToolkit = {
+  getBoxCells(matrix, boxIndex) {
+    const startRowIndex = Math.floor(boxIndex / 3) * 3;
+    const startColIndex = boxIndex % 3 * 3;
+    const result = [];
+    
+    for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+      const rowIndex = startRowIndex + Math.floor(cellIndex / 3);
+      const colIndex = startColIndex + cellIndex % 3;
+      result.push(matrix[rowIndex][colIndex]);
+    }
+    return result;
+  },
+  covertToBoxIndex(rowIndex, colIndex) {
+    return {
+      boxIndex: Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3),
+      cellIndex: rowIndex % 3 * 3 + colIndex % 3
+    }
+  },
+
+  covertFromBoxIndex(boxIndex, colIndex) {
+    return {
+      rowIndex: Math.floor(boxIndex / 3) * 3 + Math.floor(colIndex / 3),
+      colIndex: boxIndex % 3 * 3 + colIndex % 3
+    }
+  }
 }
 
 // 工具集
@@ -40,7 +79,7 @@ module.exports = class Tookit {
   /**
    * 矩阵和数据相关的功能
    */
-  static get materix() {
+  static get matrix() {
     return matrixToolkit;
   }
 
@@ -48,6 +87,6 @@ module.exports = class Tookit {
    * 宫坐标系工具
    */
   static get box() {
-    return boxToolit;
+    return boxToolkit;
   }
 };
